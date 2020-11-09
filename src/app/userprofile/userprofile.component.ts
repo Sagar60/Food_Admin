650e6ecc2e7d0b8b64279f998202de7db93ff0b8
Sagar Das <65722439+Sagar60@users.noreply.github.com>
2020-11-09 18:04:25 +0530

    Update userprofile.component.ts

--- src/app/userprofile/userprofile.component.ts
+++ src/app/userprofile/userprofile.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../shared/user.service';
 import { Router } from '@angular/router';
-import swal from 'sweetalert';
+//import swal from 'sweetalert';
 
 // npm install sweetalert --save  
 
